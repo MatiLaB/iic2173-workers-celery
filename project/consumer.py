9f05f06b5850092b7d994a23fc84ec91840bf036
JavieraCastilloAlarcon5 <jacastillo5@uc.cl>
2025-06-02 15:29:37 -0400

    arreglando consumer

--- project/consumer.py
+++ project/consumer.py
@@ -1,14 +1,11 @@
 import os
-
 from celery import Celery
+from celery_config import config 
 
 celery_app = Celery(
-    __name__,
-    # https://docs.celeryq.dev/en/stable/getting-started/backends-and-brokers/index.html
-    broker=os.environ.get('CELERY_BROKER_URL', ''),
-    backend=os.environ.get('CELERY_RESULT_BACKEND', '')
+    'worker',
+    broker=config.broker_url,
+    backend=config.result_backend
 )
 
-# Setup to use all the variables in settings
-# that begins with 'CELERY_'
 celery_app.config_from_object('celery_config.config', namespace='CELERY')
