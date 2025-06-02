fc1222c10052e94609a4d44ec767b7861cc4b54d
JavieraCastilloAlarcon5 <jacastillo5@uc.cl>
2025-06-01 23:49:10 -0400

    jobmaster conectado a redis

--- project/main.py
+++ project/main.py
@@ -32,17 +32,18 @@ app = FastAPI(
 origins = [
     "http://localhost",
     "http://localhost:3000", 
-    "http://localhost:3002",
-    "http://localhost:3001", 
+    "http://localhost:3001",
+    "http://localhost:3002", 
+    "http://127.0.0.1:3002",
 
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,      
-    allow_credentials=True,      
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"], 
-    allow_headers=["*"],        
+    allow_headers=["*"],
 )
 
 # Modelo de Pydantic para la entrada del POST /job
