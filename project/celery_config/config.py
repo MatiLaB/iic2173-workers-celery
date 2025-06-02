f6c60e6322c1cb02c5a15dfee983f3af69a0bf7c
JavieraCastilloAlarcon5 <jacastillo5@uc.cl>
2025-06-02 14:56:22 -0400

    arreglando config

--- project/celery_config/config.py
+++ project/celery_config/config.py
@@ -1,28 +1,36 @@
-# celery
 from celery.schedules import crontab
-import os
 
-REDIS_HOST = os.getenv("REDIS_HOST", "redis")
-REDIS_PORT = os.getenv("REDIS_PORT", "6379")
-REDIS_DB = os.getenv("REDIS_DB", "0")
+from . import database
 
 # https://docs.celeryq.dev/en/3.1/configuration.html
 accept_content = ['application/json']
 CELERY_SERIALIZER = 'json'
 result_serializer = 'json'
 
-broker_url = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}'
-result_backend = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}'
+# --- Usa las variables importadas de database.py ---
+broker_url = f'redis://{database.REDIS_HOST}:{database.REDIS_PORT}/{database.REDIS_DB}'
+result_backend = f'redis://{database.REDIS_HOST}:{database.REDIS_PORT}/{database.REDIS_DB}'
+# ----------------------------------------------------
 
 # Configure Celery to use a custom time zone.
 timezone = 'America/Santiago'
 # A sequence of modules to import when the worker starts
 imports = ('celery_config.tasks', )
+
+
+broker_connection_retry_on_startup = True
+broker_connection_max_retries = 30
+broker_connection_timeout = 300
+broker_pool_limit = 10
+task_acks_late = True
+worker_prefetch_multiplier = 1
+
+
 # beat scheduler
 CELERY_BEAT_SCHEDULE = {
     'every-1-minutes_add': {
         'task': 'celery_config.tasks.add',
-        'schedule': crontab(minute='*/1'), # every 1 minute
+        'schedule': crontab(minute='*/1a'), # every 1 minute
         'args': (16, 16),
     },
 }
